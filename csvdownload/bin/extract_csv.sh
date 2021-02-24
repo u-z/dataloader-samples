67bc680dcc951c79f10999b5536da3ae21b17566
Okazaki Yuji <zaki@mbf.nifty.com>
2021-02-24 09:44:54 +0900

    docker-compose.yml追加，必要のない行を削除

--- csvdownload/bin/extract_csv.sh
+++ csvdownload/bin/extract_csv.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 cd "`dirname "$0"`"
-if [ x$DL_ROOT = x ] ; then
-    DL_ROOT=$DL_BIN_DIR/..
-fi
-CONF_DIR=$DL_ROOT/conf
+
 function dl_process () {
     # Dataloader のプロセスを実行する
     echo process: $1
-    java -cp $DATALOADER_CLASSPATH -Dsalesforce.config.dir=$CONF_DIR \
+    java -cp $DATALOADER_CLASSPATH -Dsalesforce.config.dir=$DL_ROOT/conf \
         com.salesforce.dataloader.process.ProcessRunner process.name=$1 # > /dev/null
 }
 
 dl_process csvAccountExtractProcess
 
-cat /opt/dataloader/data/accoutExtract.csv
+# cat /opt/dataloader/data/accoutExtract.csv
 
